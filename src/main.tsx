17b2e901997aa3350da9a14931987be168c1dccc
designDefined <designdefined.by@gmail.com>
2024-02-10 04:11:57 +0900

    refact: Delete unused css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
