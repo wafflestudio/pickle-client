63581130c53b47729199635143f888e0f8890735
designDefined <designdefined.by@gmail.com>
2024-02-10 04:17:04 +0900

    refact: Move react-strictmode to top level

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./utils/react-query/queryClient.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </React.StrictMode>
-  </QueryClientProvider>,
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
