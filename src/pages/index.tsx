b69a82284fd7261ceb55a662d13e44e5d3e9ca1e
designDefined <designdefined.by@gmail.com>
2024-02-10 04:00:28 +0900

    feat: Setup emotion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,23 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
 export default function Home() {
-  return <main>예지누나 화이팅~</main>;
+  return (
+    <Main>
+      <h1 css={heading}>예지누나 화이팅~</h1>
+    </Main>
+  );
 }
+
+const Main = styled.main`
+  position: relative;
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+`;
+
+const heading = css`
+  font-size: 2rem;
+  font-weight: 700;
+  font-family: sans-serif;
+`;
