984d2c9bb0330baeafa3e7b0291f4cc056a954c6
designDefined <designdefined.by@gmail.com>
2024-02-10 03:51:45 +0900

    feat: Setup router & query

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/index.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./utils/react-query/queryClient.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </QueryClientProvider>,
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-function App() {
+export default function Home() {
   return <main>예지누나 화이팅~</main>;
 }
-
-export default App;
