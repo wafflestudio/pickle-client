d66ed765bf659d3a0060fa48e80aaee5dc7f4a6c
designDefined <designdefined.by@gmail.com>
2024-02-15 14:26:52 +0900

    fix: Disable react-strictmode for test

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@
  * Main에서는 React, React-Router-Dom, React-Query와 같은 라이브러리들의 필수적인 래퍼 컴포넌트만을 렌더합니다.
  */
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/index.tsx";
@@ -12,9 +11,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./utils/react-query/queryClient.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
 );
