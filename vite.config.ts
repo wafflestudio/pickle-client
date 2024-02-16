8278fd06352f7d537efc860f7c0559a96c91f0bd
lerrybe <lerry597@gmail.com>
2024-02-16 20:54:44 +0900

    chore: setting pwa options

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [""],
+      manifest: {
+        name: "seeya",
+        short_name: "seeya",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/icon_144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "icons/icon_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
