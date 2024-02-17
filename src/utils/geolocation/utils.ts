27a9cd52b2bd20fd1be883d0de7f8cde28cd7d74
Minkyu Lee <whizkyu@snu.ac.kr>
2024-02-18 04:45:55 +0900

    fix: 좌표 타입 간소화

--- src/utils/geolocation/utils.ts
+++ src/utils/geolocation/utils.ts
@@ -1,12 +1,17 @@
+type Coords = {
+  latitude: number;
+  longitude: number;
+};
+
 export function deg_to_rad(deg: number) {
   return deg * (Math.PI / 180);
 }
 
-export function distance(coord1: GeolocationCoordinates, coord2: GeolocationCoordinates) {
-  const lat1 = deg_to_rad(coord1.latitude);
-  const lat2 = deg_to_rad(coord2.latitude);
-  const lon1 = deg_to_rad(coord1.longitude);
-  const lon2 = deg_to_rad(coord2.longitude);
+export function distance(coords1: Coords, coords2: Coords) {
+  const lat1 = deg_to_rad(coords1.latitude);
+  const lat2 = deg_to_rad(coords2.latitude);
+  const lon1 = deg_to_rad(coords1.longitude);
+  const lon2 = deg_to_rad(coords2.longitude);
   return 6371000 * Math.acos(
     Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1) +
     Math.sin(lat1) * Math.sin(lat2)
