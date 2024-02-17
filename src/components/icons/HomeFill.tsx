3da30b4d9e4a962a6f3241c218be2f26d69efdc7
lerrybe <lerry597@gmail.com>
2024-02-17 19:02:16 +0900

    feat: add nav icons

---
+++ src/components/icons/HomeFill.tsx
@@ -0,0 +1,23 @@
+import { HTMLAttributes } from "react";
+
+interface Props extends HTMLAttributes<SVGElement> {
+  color?: string;
+}
+
+export default function HomeFillIcon(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      {...props}
+    >
+      <path
+        d="M6.66675 26.6666V12.6667L16.0001 5.61536L25.3334 12.6667V26.6666H18.4103V18.1538H13.5898V26.6666H6.66675Z"
+        fill={props?.color || "#09090B"}
+      />
+    </svg>
+  );
+}
